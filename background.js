75ccab798faa48c4def307addd60df06e8f924cd
Omkar Amrit Surve <47293371+iomkarsurve@users.noreply.github.com>
2020-06-09 17:51:49 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,18 @@
+const defaultFilter = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*",
+    
+]
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details){return { cancel : true} },
+    { urls: defaultFilter},
+    ["blocking"]
+)
